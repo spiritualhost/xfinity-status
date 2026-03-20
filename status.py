613cf8740e611127d1d25b6a26394276d841d202
spiritualhost <burntfurniture@gmail.com>
2026-03-20 10:02:53 -0400

    Perform address confirmation and config write; cleanup; update .gitignore; added bones for current_status scrape function

--- status.py
+++ status.py
@@ -8,8 +8,6 @@ from playwright.async_api import async_playwright, Page
 def validate_addr(address: str) -> tuple:
     try:
         tagged_addr = usaddress.tag(address)
-        print(tagged_addr)
-        print(tagged_addr[0]["AddressNumber"])
 
         #Check that address number and street name are present at minimum
         return tagged_addr if (tagged_addr[0]["AddressNumber"] and tagged_addr[0]["StreetName"]) else 1
@@ -20,8 +18,26 @@ def validate_addr(address: str) -> tuple:
         sys.exit(1)
 
 #Write the address to the config file
-def addr_to_conf(address: tuple):
-    return 0
+async def addr_to_conf(address: str) -> str:
+    if not await questionary.confirm(f"\nConfirm the address returned by the Xfinity portal is valid: {address} (y/n)").ask_async():
+        print("Error: please retry address entry.")
+        sys.exit()
+    else:
+        try:
+            config = configparser.ConfigParser()
+            config.read("config.ini")
+            
+            #Set up and write to config
+            config["Settings"] = {"address": address}
+
+            with open("config.ini", "w") as configfile:
+                config.write(configfile)
+            
+            print(f"Address {address} written to config successfully.")
+
+        except Exception as e:
+            print(f"Config error: {e}")
+            sys.exit(1)
 
 #Loading animation
 async def loading(default_message="Loading, please wait"):
@@ -32,12 +48,17 @@ async def loading(default_message="Loading, please wait"):
             sys.stdout.flush()
             await asyncio.sleep(.1)
 
+#Get current status from Xfinity
+async def current_status(page: Page) -> str:
+    return 0
+
 #Enter address
 async def enter_addr(page: Page, address: str):
-    #Set up loading animation for confirmation of page reached
-    load_ani = asyncio.create_task(loading("Page reached, attempting to enter provided address, please wait..."))
-
+ 
     try:      
+        #Set up loading animation for confirmation of page reached
+        load_ani = asyncio.create_task(loading("Page reached, attempting to enter provided address, please wait..."))
+
         #Wait for combobox to load fully, click into it, then enter input service address as a string
         await page.wait_for_selector('[role="combobox"]')
         await page.get_by_role('combobox', name="Enter service address").click()
@@ -52,7 +73,16 @@ async def enter_addr(page: Page, address: str):
 
         #Grab the canonical stored value that Xfinity returned for comparison to the originally user-entered address
         resolved = await page.get_by_test_id("serviceAddress0").inner_text()
-        print(resolved)
+
+        load_ani.cancel()
+        #Clean up task
+        try:
+            await load_ani
+        except asyncio.CancelledError:
+            pass
+
+        #Confirm that address on page is correct, write to config
+        await addr_to_conf(resolved)
 
         #Select first address on page
         await page.get_by_test_id("serviceAddress0").click()
@@ -60,16 +90,6 @@ async def enter_addr(page: Page, address: str):
     except Exception as e:
         print(f"Error on address entry: {e}")
         sys.exit(1)
-    
-    finally:
-        #Cut off animation when address entered
-        load_ani.cancel()
-        
-        #Clean up task
-        try:
-            await load_ani
-        except asyncio.CancelledError:
-            pass
 
 #Asynchronously open the web browser
 async def open_browser(address: str):
@@ -93,16 +113,11 @@ async def open_browser(address: str):
 
         await browser.close()
 
-
-
 if __name__ == "__main__":
 
     #Get service address for business and perform initial validation
-    serv_addr = input("What is the service address for the business, including the zip code: ")
+    serv_addr = input("What is the service address for the business, including the zip code: ").strip()
     returned_valid = validate_addr(serv_addr)
-    
-    #Update config file
-    addr_to_conf(returned_valid)
 
     #Open a headless web browser
     asyncio.run(open_browser(serv_addr))
